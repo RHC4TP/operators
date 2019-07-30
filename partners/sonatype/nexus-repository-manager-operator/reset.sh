a2118c13097c047a93cdca52e1cfbbf8c86622be
Daniel <dhoover@redhat.com>
2019-07-30 14:32:13 -0400

    fixing directory structure

--- partners/sonatype/nexus-repository-manager-operator/reset.sh
+++
@@ -1,6 +0,0 @@
-oc delete nexus nexus-operator
-oc delete deployment nexus-operator
-git pull
-operator-sdk build quay.io/dhoover103/repomgr-operator:v0.0.1
-docker push quay.io/dhoover103/repomgr-operator:v0.0.1
-
